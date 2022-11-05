8acf9a771ac654bd0e3d0311437a356d81d27111
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2022-11-05 09:10:16 -0400

    feat: add dynamic count

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 interface HomeProps {
-  count: number;
+  poolCount: number;
+  guessCount: number;
+  userCount: number;
 }
 
 import Image from "next/image";
@@ -7,46 +9,61 @@ import appPreviewImg from "../assets/app-nlw-copa-preview.png";
 import logoImg from "../assets/logo.svg";
 import usersAvatarExampleImg from "../assets/users-avatar-example.png";
 import iconCheckImg from "../assets/icon-check.svg";
+import { api } from "../lib/axios";
 
 export default function Home(props: HomeProps) {
   return (
-    <div>
+    <div className="max-w-[1124px] h-screen mx-auto grid grid-cols-2 items-center gap-28">
       <main>
         <Image src={logoImg} alt="NLW Copa" quality={100} />
 
-        <h1>Crie seu próprio bolão da copa e compartilhe entre amigos!</h1>
+        <h1 className="mt-14 text-white text-5xl font-bold leading-tight">
+          Crie seu próprio bolão da copa e compartilhe entre amigos!
+        </h1>
 
-        <div>
+        <div className="mt-10 flex items-center gap-2">
           <Image src={usersAvatarExampleImg} alt="" quality={100} />
-        </div>
-        <strong>
-          <span>+12.592</span> pessoas já estão usando
-        </strong>
 
-        <form>
-          <input type="text" required placeholder="Qual o nome do seu bolão" />
-          <button type="submit">Criar meu bolão</button>
+          <strong className="text-gray-100 text-xl">
+            <span className="text-ignite-500">+{props.userCount}</span> pessoas já estão
+            usando
+          </strong>
+        </div>
+        <form className="mt-10 flex gap-2">
+          <input
+            className="flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm outline-none text-gray-100"
+            type="text"
+            required
+            placeholder="Qual o nome do seu bolão"
+          />
+
+          <button
+            className="bg-yellow-500 px-6 py-4 rounded text-gray-900 font-bold text-sm uppercase hover:bg-yellow-700"
+            type="submit"
+          >
+            Criar meu bolão
+          </button>
         </form>
 
-        <p>
+        <p className="mt-4 text-sm text-gray-300 leading-relaxed">
           Após criar seu bolão, você receberá um código único que poderá usar
           para convidar outras pessoas 🚀
         </p>
 
-        <div>
-          <div>
+        <div className="mt-10 pt-10 border-t border-gray-600  flex items-center justify-between text-gray-100">
+          <div className="flex items-center gap-6">
             <Image src={iconCheckImg} alt="" />
-            <div>
-              <span>+2.034</span>
+            <div className="flex flex-col">
+              <span className="font-bold text-2xl">+{props.poolCount}</span>
               <span>Bolões criados</span>
             </div>
           </div>
-
-          <div>
+          <div className="w-px h-14 bg-gray-600"></div>
+          <div className="flex items-center gap-6">
             <Image src={iconCheckImg} alt="" />
-            <div>
-              <span>+2.034</span>
-              <span>Bolões criados</span>
+            <div className="flex flex-col">
+              <span className="font-bold text-2xl">+{props.guessCount}</span>
+              <span>Palpites enviados</span>
             </div>
           </div>
         </div>
@@ -61,13 +78,18 @@ export default function Home(props: HomeProps) {
 }
 
 export const getServerSideProps = async () => {
-  const response = await fetch("http://localhost:3333/pools/count");
-  const data = await response.json();
-  console.log(data);
+
+  const [poolCountResponse, guessCountResponse, userCountResponse ] = await Promise.all([
+    api.get("/pools/count"),
+    api.get("/guesses/count"),
+    api.get("/users/count")
+  ])
 
   return {
     props: {
-      count: data.count,
+      poolCount: poolCountResponse.data.count,
+      guessCount: guessCountResponse.data.count,
+      userCount: userCountResponse.data.count
     },
   };
 };
